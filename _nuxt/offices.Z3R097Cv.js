22a3779146d1f7cf8b9ff28bd71eb2edb6608729
zuhair2025 <zuhair2025@gmail.com>
2024-02-02 21:36:57 +0000

    Deploying to gh-pages from @ zuhair2025/khatiyan-nuxt-app@2b7979dd5eb1d95b18bba277bdccc9268b3078f8 🚀

--- _nuxt/offices.Z3R097Cv.js
+++
@@ -1,1 +0,0 @@
-import{M as t}from"./MainLayout.9vNNa0Jc.js";import{f as o,w as a,o as e,a as r}from"./entry.DayLWYYp.js";import"./nuxt-link.ZhjFNTKt.js";import"./Icon.9ZigmBsT.js";import"./index.0wqzsba0.js";const s=r("h1",null,"OFFICES",-1),u={__name:"offices",setup(c){return(_,i)=>(e(),o(t,null,{default:a(()=>[s]),_:1}))}};export{u as default};
