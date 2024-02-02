21ca0660e5e800879c7047dd92c38adec40658eb
zuhair2025 <zuhair2025@gmail.com>
2024-02-03 03:14:06 +0600

    modified offices page

--- pages/offices.vue
+++ pages/offices.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-
-    </div>
+    <MainLayout>
+        <h1>OFFICES</h1>
+    </MainLayout>
 </template>
 
 <script setup>
-
+import MainLayout from '~/layouts/MainLayout.vue';
 </script>
